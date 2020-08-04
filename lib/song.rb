f05f0e0c47b9a157575c1f8a78815d860c3ce296
Nerly Ton <nerlyton@gmail.com>
2020-08-04 19:37:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,6 +55,9 @@ class Song
   def self.create_from_filename(name)
     # self.new_from_filename(name)
     @@all << self.new_from_filename(name)
-    
+  end
+  
+  def self.destroy_all
+    @@all.clear
   end
 end
