0282066dc266ffd2e7a717cd5a2cdc19e212e5d1
Nerly Ton <nerlyton@gmail.com>
2020-08-04 19:35:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,10 +49,12 @@ class Song
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
+    song
+  end
+  
+  def self.create_from_filename(name)
+    self.new_from_filename(name)
     @@all << song
     song
   end
-    
-      
-
 end
