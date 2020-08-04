3e3acf27bea8de420fd614a4d6f23c4fe9aafe21
Nerly Ton <nerlyton@gmail.com>
2020-08-04 18:36:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,9 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+ def self.create
+   @@all << self.new
+ 
   def self.all
     @@all
   end
@@ -10,10 +13,10 @@ class Song
     self.class.all << self
   end
   
-  def self.create
-    song = self.new 
-    # song.name = name
-   return  @@all << song
-  end
+  # def self.create
+  #   song = self.new 
+  #   # song.name = name
+  # return  @@all << song
+  # end
 
 end
